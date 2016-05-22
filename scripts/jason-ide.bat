e6af9633060cdf5564263fc7bc3b94417ad6bf23
Jomi F. Hubner <jomifred@gmail.com>
2016-05-22 21:53:48 +0200

    test release task

--- scripts/jason-ide.bat
+++ scripts/jason-ide.bat
@@ -1,5 +1,5 @@
 @echo off
 rem SET JAVA_HOME=C:\Program Files\jdk1.5
 rem SET PATH="%JAVA_HOME%\bin";%PATH%
-java -cp ..\lib\jason.jar;..\lib\jade.jar;jedit\jedit.jar;..\lib\cartago.jar;..\lib\c4jason.jar;..\lib\moise.jar;..\lib\jacamo.jar;%CLASSPATH% org.gjt.sp.jedit.jEdit
+java -cp ..\libs\jason.jar;..\libs\jade.jar;..\jedit\jedit.jar org.gjt.sp.jedit.jEdit
 
