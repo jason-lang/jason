def33feeb30db1cf5ed5d944d905d32c68bbeeab
Jomi F. Hubner <jomifred@gmail.com>
2017-04-06 15:33:36 -0300

    add syntax highlight for jacamo code in asciidoc doc

---
+++ src/main/resources/pygments_init.rb
@@ -0,0 +1,14 @@
+require 'pygments'
+
+# use a custom Pygments installation (directory that contains pygmentize)
+Pygments.start '$HOME/tmp/pygments'
+
+# example of registering a missing or additional lexer
+Pygments::Lexer.create name: 'JasonAgent', aliases: ['jasonagent'],
+    filenames: ['*.asl'], mimetypes: ['text/x-jason']
+
+Pygments::Lexer.create name: 'JasonProject', aliases: ['jasonproject'],
+    filenames: ['*.mas2j'], mimetypes: ['text/x-jasonproject']
+
+Pygments::Lexer.create name: 'JaCaMoProject', aliases: ['jacamoproject'],
+    filenames: ['*.jcm'], mimetypes: ['text/x-jacamoproject']
