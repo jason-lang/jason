5d38a023b3abedc953aec4b6753da92e7dd53fd3
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:49 -0300

    TestPlanFailure.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/test/java/test/asunit/TestPlanFailure.java
+++ src/test/java/test/asunit/TestPlanFailure.java
@@ -7,38 +7,38 @@ import org.junit.Test;
 
 public class TestPlanFailure {
 
-    TestAgent ag; 
+    TestAgent ag;
 
     // initialisation of the agent test
     @Before
     public void setupAg() {
         ag = new TestAgent();
-        
+
         // defines the agent's AgentSpeak code
         ag.parseAScode(
-                "+!test  : true <- !g1(X); endtest; end(X). "+
+            "+!test  : true <- !g1(X); endtest; end(X). "+
 
-                "+!g1(X) : true <- inig1; !g2(X); endg1. "+
-                "+!g2(X) : true <- inig2; !g3(X); endg2. "+
-                "+!g3(X) : true <- inig2; !g4(X); endg3. "+
-                "+!g4(X) : true <- inig4; !g5(X); endg4. "+
-                "+!g5(_) : true <- .fail. "+
-                "-!g3(failure)[error(ia_failed)] : true <- infailg3. "+
-                
-                "+!norel <- !j; endnorel. "+
-                "-!j[error(no_relevant)] <- infailj. " +
+            "+!g1(X) : true <- inig1; !g2(X); endg1. "+
+            "+!g2(X) : true <- inig2; !g3(X); endg2. "+
+            "+!g3(X) : true <- inig2; !g4(X); endg3. "+
+            "+!g4(X) : true <- inig4; !g5(X); endg4. "+
+            "+!g5(_) : true <- .fail. "+
+            "-!g3(failure)[error(ia_failed)] : true <- infailg3. "+
 
-                "+!s <- ?bufferReady; jason.asunit.print(ok)." +
-                "+?bufferReady : a     <- jason.asunit.print(oka). " +
-                "+?bufferReady : not a <- .fail.  " +
-                "-?bufferReady <- jason.asunit.print(w); ?bufferReady."
+            "+!norel <- !j; endnorel. "+
+            "-!j[error(no_relevant)] <- infailj. " +
+
+            "+!s <- ?bufferReady; jason.asunit.print(ok)." +
+            "+?bufferReady : a     <- jason.asunit.print(oka). " +
+            "+?bufferReady : not a <- .fail.  " +
+            "-?bufferReady <- jason.asunit.print(w); ?bufferReady."
 
         );
     }
-    
+
     @Test(timeout=2000)
     public void testFailurePlan() {
-        ag.addGoal("test");      
+        ag.addGoal("test");
         ag.assertAct("inig4", 10);
         ag.assertAct("infailg3", 5);
         ag.assertAct("endg2", 5);
@@ -48,14 +48,14 @@ public class TestPlanFailure {
 
     @Test(timeout=2000)
     public void testNoRelPlan() {
-        ag.addGoal("norel");      
+        ag.addGoal("norel");
         ag.assertAct("infailj", 10);
         ag.assertAct("endnorel", 5);
     }
 
     @Test(timeout=2000)
     public void testTestGoalFailure() {
-        ag.addGoal("s");      
+        ag.addGoal("s");
         ag.assertPrint("w", 10);
         ag.assertPrint("w", 10);
         ag.assertPrint("w", 10);
