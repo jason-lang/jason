68456ef6a88ac6a09ad995beb3233e535d5dd8cb
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:46 -0300

    abolish.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/stdlib/abolish.java
+++ src/main/java/jason/stdlib/abolish.java
@@ -9,15 +9,15 @@ import jason.asSyntax.Term;
 
 /**
   <p>Internal action: <b><code>.abolish</code></b>.
-  
+
   <p>Description: removes all beliefs that match the argument. As for the
-  "-" operator, an event will be generated for each deletion. 
+  "-" operator, an event will be generated for each deletion.
   Different from the "-" operator, which consider "self" as the default source, .abolish will ignore the source if not informed.
-  
+
   <p>Parameters:<ul>
   <li>+ pattern (literal or variable): the "pattern" for what should be removed.<br/>
   </ul>
-  
+
   <p>Examples:<ul>
   <li> <code>.abolish(b(_))</code>: remove all <code>b/1</code> beliefs, regardless of the argument value and the source of the belief.</li>
   <li> <code>.abolish(c(_,t))</code>: remove all <code>c/2</code> beliefs where the second argument is <code>2</code>.</li>
@@ -31,20 +31,24 @@ import jason.asSyntax.Term;
  */
 public class abolish extends DefaultInternalAction {
 
-    @Override public int getMinArgs() { return 1; }
-    @Override public int getMaxArgs() { return 1; }
+    @Override public int getMinArgs() {
+        return 1;
+    }
+    @Override public int getMaxArgs() {
+        return 1;
+    }
 
     @Override protected void checkArguments(Term[] args) throws JasonException {
         super.checkArguments(args); // check number of arguments
         if (!args[0].isLiteral() & !args[0].isVar())
             throw JasonException.createWrongArgument(this,"first argument must be a literal or variable.");
     }
-    
+
     @Override public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
         checkArguments(args);
 
         ts.getAg().abolish((Literal)args[0], un);
         return true;
     }
-    
+
 }
