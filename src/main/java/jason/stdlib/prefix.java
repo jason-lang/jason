5b366939f700291cda0fd2fd42a5c51ffc352484
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:47 -0300

    prefix.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/stdlib/prefix.java
+++ src/main/java/jason/stdlib/prefix.java
@@ -15,19 +15,19 @@ import java.util.List;
 /**
 
   <p>Internal action: <b><code>.prefix(<i>P</i>,<i>L</i>)</code></b>.
-  
+
   <p>Description: checks if some list <i>P</i> is a prefix of list <i>L</i>. If
   <i>P</i> has free variables, this internal action backtracks all
   possible values for <i>P</i>.
 
   <p>Parameters:<ul>
-  
+
   <li>+/- prefix (list): the prefix to be checked.</li>
   <li>+ list (list): the list where the prefix is from.</li>
-  
+
   </ul>
 
-  <p>Examples:<ul> 
+  <p>Examples:<ul>
 
   <li> <code>.prefix([a],[a,b,c])</code>: true.</li>
   <li> <code>.prefix([b,c],[a,b,c])</code>: false.</li>
@@ -53,25 +53,29 @@ import java.util.List;
 
 */
 public class prefix extends DefaultInternalAction {
-    
+
     private static final long serialVersionUID = -4736810884249871078L;
     private static InternalAction singleton = null;
     public static InternalAction create() {
-        if (singleton == null) 
+        if (singleton == null)
             singleton = new prefix();
         return singleton;
     }
 
     // Needs exactly 2 arguments
-    @Override public int getMinArgs() { return 2; }
-    @Override public int getMaxArgs() { return 2; } 
+    @Override public int getMinArgs() {
+        return 2;
+    }
+    @Override public int getMaxArgs() {
+        return 2;
+    }
 
     // improve the check of the arguments to also check the type of the arguments
     @Override protected void checkArguments(Term[] args) throws JasonException {
         super.checkArguments(args); // check number of arguments
         if (!args[0].isList() && !args[0].isVar())
             throw JasonException.createWrongArgument(this,"first argument must be a list or a variable");
-        if (!args[1].isList()) 
+        if (!args[1].isList())
             throw JasonException.createWrongArgument(this,"second argument must be a list");
     }
 
@@ -84,15 +88,15 @@ public class prefix extends DefaultInternalAction {
 
         final Term sublist = args[0];
         final List<Term> list = ((ListTerm)args[1]).getAsList(); // use a Java List for better performance in remove last
-        
+
         return new Iterator<Unifier>() {
             Unifier c = null; // the current response (which is an unifier)
             boolean triedEmpty = false;
-            
+
             public boolean hasNext() {
-                if (c == null) // the first call of hasNext should find the first response 
+                if (c == null) // the first call of hasNext should find the first response
                     find();
-                return c != null; 
+                return c != null;
             }
 
             public Unifier next() {
@@ -101,7 +105,7 @@ public class prefix extends DefaultInternalAction {
                 find(); // find next response
                 return b;
             }
-            
+
             void find() {
                 while (!list.isEmpty()) {
                     ListTerm candidate = ASSyntax.createList(list);
@@ -116,9 +120,9 @@ public class prefix extends DefaultInternalAction {
                     c = un.clone();
                     if (c.unifiesNoUndo(sublist, ASSyntax.createList())) {
                         return; // found another sublist, c is the current response
-                    }                   
+                    }
                 }
-                c = null; // no more sublists found 
+                c = null; // no more sublists found
             }
 
             public void remove() {}
