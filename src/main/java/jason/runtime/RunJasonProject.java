da8ee5dd71c0db4c2b329d68d0286b038af56e42
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:46 -0300

    RunJasonProject.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/runtime/RunJasonProject.java
+++ src/main/java/jason/runtime/RunJasonProject.java
@@ -9,61 +9,61 @@ import jason.util.Config;
 
 /**
  * Run a Jason mas2j project
- * 
- * parameters: 
+ *
+ * parameters:
  *    /Jason mas2j Project File/
- * 
+ *
  * @author jomi
  *
  */
 public class RunJasonProject {
-    
+
     static MASLauncherInfraTier launcher;
-    
+
     // Run the parser
     public static void main (String args[]) {
 
-      String name;
-      mas2j parser;
-      MAS2JProject project = new MAS2JProject();
-      
-      if (args.length == 0) {
-          System.out.println("usage must be:");
-          System.out.println("      java "+RunJasonProject.class.getName()+" <MAS2j Project File>");
-          return;
-      } else {
-          name = args[0];
-          System.err.println("reading from file " + name + " ..." );
-          try {
-              parser = new mas2j(new java.io.FileInputStream(name));
-          } catch(java.io.FileNotFoundException e){
-              System.err.println("file \"" + name + "\" not found.");
-              return;
-          }
-      }
-      
-      // parsing
-      try {
-          File file = new File(name);
-          project = parser.mas();
-          if (Config.get().getJasonJar() == null) {
-              System.out.println("Jason is not configured, creating a default configuration");
-              Config.get().fix();
-          }
-          project.setProjectFile(file);
-          project.setDirectory(file.getAbsoluteFile().getParentFile().getAbsolutePath());
-          System.out.println("file "+name+" parsed successfully!\n");
-          
-          launcher = project.getInfrastructureFactory().createMASLauncher();
-          launcher.setProject(project);
-          launcher.writeScripts(false, false);
-          
-          new Thread(launcher, "MAS-Launcher").start();
-      } catch(Exception e){
-          System.err.println("parsing errors found... \n" + e);
-      }
+        String name;
+        mas2j parser;
+        MAS2JProject project = new MAS2JProject();
+
+        if (args.length == 0) {
+            System.out.println("usage must be:");
+            System.out.println("      java "+RunJasonProject.class.getName()+" <MAS2j Project File>");
+            return;
+        } else {
+            name = args[0];
+            System.err.println("reading from file " + name + " ..." );
+            try {
+                parser = new mas2j(new java.io.FileInputStream(name));
+            } catch(java.io.FileNotFoundException e) {
+                System.err.println("file \"" + name + "\" not found.");
+                return;
+            }
+        }
+
+        // parsing
+        try {
+            File file = new File(name);
+            project = parser.mas();
+            if (Config.get().getJasonJar() == null) {
+                System.out.println("Jason is not configured, creating a default configuration");
+                Config.get().fix();
+            }
+            project.setProjectFile(file);
+            project.setDirectory(file.getAbsoluteFile().getParentFile().getAbsolutePath());
+            System.out.println("file "+name+" parsed successfully!\n");
+
+            launcher = project.getInfrastructureFactory().createMASLauncher();
+            launcher.setProject(project);
+            launcher.writeScripts(false, false);
+
+            new Thread(launcher, "MAS-Launcher").start();
+        } catch(Exception e) {
+            System.err.println("parsing errors found... \n" + e);
+        }
     }
-    
+
     public MASLauncherInfraTier getLauncher() {
         return launcher;
     }
