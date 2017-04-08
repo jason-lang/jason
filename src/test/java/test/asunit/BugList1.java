3e6e56032f01935e84a3a84705b229952211ac60
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:48 -0300

    BugList1.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/test/java/test/asunit/BugList1.java
+++ src/test/java/test/asunit/BugList1.java
@@ -7,21 +7,21 @@ import org.junit.Test;
 
 public class BugList1 {
 
-    TestAgent ag, oa, ob; 
+    TestAgent ag, oa, ob;
 
     // initialisation of the agent test
     @Before
     public void setupAg() {
         ag = new TestAgent("a");
-        
+
         // defines the agent's AgentSpeak code
         ag.parseAScode(
-                "+!start <- L=\"[a]\"; !g(L). "+ 
-                "+!g([]) <- jason.asunit.print(no)."+
-                "+!g(_) <- jason.asunit.print(yes)."
+            "+!start <- L=\"[a]\"; !g(L). "+
+            "+!g([]) <- jason.asunit.print(no)."+
+            "+!g(_) <- jason.asunit.print(yes)."
         );
     }
-    
+
     @Test(timeout=2000)
     public void testProg() {
         ag.addGoal("start");
