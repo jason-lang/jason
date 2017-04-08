9cb2d4812d11ea8280e2f3c72082fbfe8204d31b
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:42 -0300

    random.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- examples/iterated-prisoners-dilemma/my/random.java
+++ examples/iterated-prisoners-dilemma/my/random.java
@@ -13,25 +13,25 @@ import java.util.Iterator;
 
 /** backtrack version of random */
 public class random extends DefaultInternalAction {
-    
-    private Random random = new Random();    
-    
+
+    private Random random = new Random();
+
     @Override
     public Object execute(final TransitionSystem ts, final Unifier un, final Term[] args) throws Exception {
         try {
             if (!args[0].isVar()) {
-                throw new JasonException("The first argument of the internal action 'random' is not a variable.");                
+                throw new JasonException("The first argument of the internal action 'random' is not a variable.");
             }
             if (!args[1].isNumeric()) {
-                throw new JasonException("The second argument of the internal action 'random' is not a number.");                
+                throw new JasonException("The second argument of the internal action 'random' is not a number.");
             }
             final int max = (int)((NumberTerm)args[1]).solve();
 
             return new Iterator<Unifier>() {
-                
+
                 // we always have a next random number
-                public boolean hasNext() { 
-                    return ts.getUserAgArch().isRunning();  
+                public boolean hasNext() {
+                    return ts.getUserAgArch().isRunning();
                 }
 
                 public Unifier next() {
@@ -47,6 +47,6 @@ public class random extends DefaultInternalAction {
             throw new JasonException("The internal action 'random' has not received the required argument.");
         } catch (Exception e) {
             throw new JasonException("Error in internal action 'random': " + e, e);
-        }    
+        }
     }
 }
