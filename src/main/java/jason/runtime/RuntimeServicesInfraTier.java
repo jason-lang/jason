34c697febe5b41c88837a5984b98d93d879d594f
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:46 -0300

    RuntimeServicesInfraTier.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/runtime/RuntimeServicesInfraTier.java
+++ src/main/java/jason/runtime/RuntimeServicesInfraTier.java
@@ -18,26 +18,26 @@ public interface RuntimeServicesInfraTier {
      * Creates a new agent with <i>agName</i> from source
      * <i>agSource</i>, using <i>agClass</i> as agent class (default
      * value is "jason.asSemantics.Agent"), <i>archClass</i> as agent
-     * architecture class (default value is "jason.architecture.AgArch"), 
+     * architecture class (default value is "jason.architecture.AgArch"),
      * <i>bbPars</i> as the belief base
      * class (default value is "DefaultBeliefBase"), <i>stts</i> as
      * Settings (default value is new Settings()), and
      * <i>father</i> is the agent creating this agent (null is none).
-     * 
+     *
      * <p> Example: createAgent("bob", "bob.asl", "mypkg.MyAgent", null, null, null);
-     * 
+     *
      * Returns the name of the agent
      */
     public String createAgent(String agName, String agSource, String agClass, List<String> archClasses, ClassParameters bbPars, Settings stts, Agent father) throws Exception;
 
     /** start (e.g. create thread) the agent */
     public void startAgent(String agName);
-    
+
     /**
      * Clones an agent
-     * 
+     *
      * @param source: the used as source of beliefs, plans, ...
-     * @param archClassName: the architecture that will be used 
+     * @param archClassName: the architecture that will be used
      * @param agName: the name of the clone
      * @return the agent arch created
      * @throws JasonException
@@ -45,7 +45,7 @@ public interface RuntimeServicesInfraTier {
     public AgArch clone(Agent source, List<String> archClasses, String agName) throws JasonException;
 
     /**
-     * Kills the agent named <i>agName</i> as a requested by <i>byAg</i>. 
+     * Kills the agent named <i>agName</i> as a requested by <i>byAg</i>.
      * The stopAg() method, in
      * the agent architecture is called before the agent is removed.
      */
