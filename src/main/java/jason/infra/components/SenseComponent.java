e8496360eb7cb21bfa6cb0a01e2b17101493c927
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:45 -0300

    SenseComponent.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/infra/components/SenseComponent.java
+++ src/main/java/jason/infra/components/SenseComponent.java
@@ -3,50 +3,50 @@ package jason.infra.components;
 import jason.infra.centralised.CentralisedAgArchAsynchronous;
 
 public class SenseComponent extends AgentComponent {
-    
+
     public SenseComponent(CentralisedAgArchAsynchronous centralisedAgArchAsynchronous) {
         super(centralisedAgArchAsynchronous);
     }
-    
-    public boolean canSleep() { 
-        return ag.getTS().canSleepSense(); 
+
+    public boolean canSleep() {
+        return ag.getTS().canSleepSense();
     }
-    
+
     public void wakeUp() {
         synchronized (ag.objSense) {
             if (sleeping) {
                 sleeping = false;
-                enqueueExecutor(false);             
+                enqueueExecutor(false);
             }
         }
     }
-    
+
     /*
     public void wakeUp(boolean ts) {
         synchronized (ag.objSense) {
             if (ts || sleeping) {
                 sleeping = false;
-                enqueueExecutor(ts);                
+                enqueueExecutor(ts);
             }
         }
     }*/
-    
+
     public void enqueueExecutor(boolean ts) {
         if (!inQueue || ts) {
             inQueue = true;
             ag.getExecutorSense().execute(this);
         }
     }
-    
+
     public void run() {
         int cycles = ag.getCyclesSense();
         //int number_cycles = 1;
         int i = 0;
-        
+
         while (ag.isRunning() && i < cycles) {
             i++;
             ag.getTS().sense();
-            
+
             synchronized (ag.objSense) {
                 if (canSleep()) {
                     inQueue = false;
