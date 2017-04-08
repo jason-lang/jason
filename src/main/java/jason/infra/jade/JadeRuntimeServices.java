0b829d6416e28f76d96c058843f59c78c04a253a
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:45 -0300

    JadeRuntimeServices.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/infra/jade/JadeRuntimeServices.java
+++ src/main/java/jason/infra/jade/JadeRuntimeServices.java
@@ -24,39 +24,39 @@ import java.util.logging.Logger;
 public class JadeRuntimeServices implements RuntimeServicesInfraTier {
 
     private static Logger logger  = Logger.getLogger(JadeRuntimeServices.class.getName());
-    
+
     private ContainerController cc;
-    
+
     private Agent jadeAgent;
-    
+
     JadeRuntimeServices(ContainerController cc, Agent ag) {
         this.cc = cc;
         jadeAgent = ag;
     }
-    
+
     public String createAgent(String agName, String agSource, String agClass, List<String> archClasses, ClassParameters bbPars, Settings stts, jason.asSemantics.Agent father) throws Exception {
         try {
             if (logger.isLoggable(Level.FINE)) {
                 logger.fine("Creating jade agent " + agName + "from source " + agSource + "(agClass=" + agClass + ", archClass=" + archClasses + ", settings=" + stts);
             }
-    
+
             AgentParameters ap = new AgentParameters();
             ap.setAgClass(agClass);
             ap.addArchClass(archClasses);
             ap.setBB(bbPars);
             ap.asSource = new File(agSource);
-            
+
             if (stts == null) stts = new Settings();
-            
+
             cc.createNewAgent(agName, JadeAgArch.class.getName(), new Object[] { ap, false, false }).start();
-            
+
             return agName;
         } catch (Exception e) {
             logger.log(Level.SEVERE, "Error creating agent " + agName, e);
         }
         return null;
     }
-    
+
     public void startAgent(String agName) {
         // nothing to do, the jade create new agent is enough
     }
@@ -77,7 +77,7 @@ public class JadeRuntimeServices implements RuntimeServicesInfraTier {
             template.addServices(sd);
             DFAgentDescription[] ans = DFService.search(jadeAgent, template);
             for (int i=0; i<ans.length; i++) {
-                ags.add(ans[i].getName().getLocalName());                
+                ags.add(ans[i].getName().getLocalName());
             }
             /*
             SearchConstraints c = new SearchConstraints();
@@ -85,20 +85,20 @@ public class JadeRuntimeServices implements RuntimeServicesInfraTier {
             AMSAgentDescription[] all = AMSService.search( jadeAgent, new AMSAgentDescription(), c);
             for (AMSAgentDescription ad: all) {
                 AID agentID = ad.getName();
-                if (    !agentID.getName().startsWith("ams@") && 
+                if (    !agentID.getName().startsWith("ams@") &&
                         !agentID.getName().startsWith("df@") &&
                         !agentID.getName().startsWith(RunJadeMAS.environmentName) &&
                         !agentID.getName().startsWith(RunJadeMAS.controllerName)
                    ) {
-                    ags.add(agentID.getLocalName());                
+                    ags.add(agentID.getLocalName());
                 }
-            } 
-            */       
+            }
+            */
             return ags;
             //logger.warning("getAgentsName is not implemented yet!");
         } catch (Exception e) {
             logger.log(Level.SEVERE, "Error getting agents' name", e);
-        }        
+        }
         return null;
     }
 
@@ -117,8 +117,8 @@ public class JadeRuntimeServices implements RuntimeServicesInfraTier {
             if (ac == null) {
                 logger.warning("Agent "+agName+" does not exist!");
             } else {
-                // TODO: if (ag.getTS().getAg().canBeKilledBy(byAg)) 
-                ac.kill();    
+                // TODO: if (ag.getTS().getAg().canBeKilledBy(byAg))
+                ac.kill();
                 return true;
             }
         } catch (Exception e) {
@@ -127,18 +127,18 @@ public class JadeRuntimeServices implements RuntimeServicesInfraTier {
         return false;
     }
 
-   
+
     public void stopMAS() throws Exception {
         if (cc != null) {
             new Thread() { // this command should not block the agent!
-                public void run() {  
+                public void run() {
                     try {
                         cc.getPlatformController().kill();
                     } catch (ControllerException e) {
                         e.printStackTrace();
-                    }  
+                    }
                 }
-            }.start();
+            } .start();
         }
     }
 
