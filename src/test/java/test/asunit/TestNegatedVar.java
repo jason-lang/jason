e4375c370ed665dd09dc49ff4c3dbc30388fd9b4
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:49 -0300

    TestNegatedVar.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/test/java/test/asunit/TestNegatedVar.java
+++ src/test/java/test/asunit/TestNegatedVar.java
@@ -7,22 +7,22 @@ import org.junit.Test;
 
 public class TestNegatedVar {
 
-    TestAgent ag; 
+    TestAgent ag;
 
     // initialisation of the agent test
     @Before
     public void setupAg() {
         ag = new TestAgent();
         ag.setDebugMode(true);
-        
+
         // defines the agent's AgentSpeak code
         ag.parseAScode(
-                "+!start <- +b. " +
-                "+B <- !~B. " +
-                "+!X <- jason.asunit.print(X)." 
+            "+!start <- +b. " +
+            "+B <- !~B. " +
+            "+!X <- jason.asunit.print(X)."
         );
     }
-    
+
     @Test(timeout=2000)
     public void testContext() {
         ag.addGoal("start");
