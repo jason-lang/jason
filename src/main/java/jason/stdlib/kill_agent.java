d9d13f8c22e4e932f96ee6a541a03c3ecd3d570b
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:47 -0300

    kill_agent.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/stdlib/kill_agent.java
+++ src/main/java/jason/stdlib/kill_agent.java
@@ -8,7 +8,7 @@ import jason.asSyntax.StringTerm;
 
 /**
   <p>Internal action: <b><code>.kill_agent</code></b>.
-  
+
   <p>Description: kills the agent whose name is given as parameter. This is a
      provisional internal action to be used while more adequate mechanisms for
      creating and killing agents are being developed. In particular, note that
@@ -16,14 +16,14 @@ import jason.asSyntax.StringTerm;
      permissions, etc.! It is the programmers' responsibility to use this
      action.
 
-  
+
   <p>Parameters:<ul>
-  
+
   <li>+ name (atom or string): the name of the agent to be killed.<br/>
 
   </ul>
-  
-  <p>Example:<ul> 
+
+  <p>Example:<ul>
 
   <li> <code>.kill_agent(bob)</code>: kills the agent named bob.</li>
 
@@ -37,17 +37,21 @@ import jason.asSyntax.StringTerm;
 */
 public class kill_agent extends DefaultInternalAction {
 
-    @Override public int getMinArgs() { return 1; }
-    @Override public int getMaxArgs() { return 1; }
+    @Override public int getMinArgs() {
+        return 1;
+    }
+    @Override public int getMaxArgs() {
+        return 1;
+    }
 
     @Override
-	public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
+    public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
         checkArguments(args);
         String name;
-        if (args[0].isString()) 
+        if (args[0].isString())
             name = ((StringTerm)args[0]).getString();
         else
             name = args[0].toString();
         return ts.getUserAgArch().getRuntimeServices().killAgent(name, ts.getUserAgArch().getAgName());
-	}
+    }
 }
