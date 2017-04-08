0e9439fd71051af91a35f1c83212f24a4f0114a6
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:48 -0300

    RuleTest.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/test/java/test/RuleTest.java
+++ src/test/java/test/RuleTest.java
@@ -27,7 +27,7 @@ public class RuleTest extends TestCase {
     protected void setUp() throws Exception {
         super.setUp();
     }
- 
+
     public void testLogCons() {
         Agent ag = new Agent();
         ag.initAg();
@@ -43,7 +43,7 @@ public class RuleTest extends TestCase {
         // add r(X) :- a(X)
         Rule r = new Rule(Literal.parseLiteral("r(X)"), Literal.parseLiteral("a(X)"));
         ag.getBB().add(r);
-        
+
         Iterator<Unifier> iun = Literal.parseLiteral("r(20)").logicalConsequence(ag, new Unifier());
         assertEquals(1,iteratorSize(iun));
 
@@ -51,7 +51,7 @@ public class RuleTest extends TestCase {
         assertEquals(3,iteratorSize(iun));
 
         // add v(X) :- a(X) & X > 15 | c(X)
-        r = new Rule(Literal.parseLiteral("v(X)"), LogExpr.parseExpr("a(X) & X > 15 | c(X)"));  
+        r = new Rule(Literal.parseLiteral("v(X)"), LogExpr.parseExpr("a(X) & X > 15 | c(X)"));
         ag.getBB().add(r);
 
         iun = Literal.parseLiteral("v(30)").logicalConsequence(ag, new Unifier());
@@ -71,15 +71,15 @@ public class RuleTest extends TestCase {
         ag.getBB().add(r);
         iun = Literal.parseLiteral("s(X)").logicalConsequence(ag, new Unifier());
         assertEquals(3,iteratorSize(iun));
-        
+
         // add t(a) :- s(X)
         r = new Rule(Literal.parseLiteral("t(a)"), LogExpr.parseExpr("s(X)"));
         ag.getBB().add(r);
-        
+
         iun = Literal.parseLiteral("t(X)").logicalConsequence(ag, new Unifier());
         assertEquals(3,iteratorSize(iun));
     }
-    
+
     public void testAdd() throws ParseException {
         Agent ag = new Agent();
         ag.initAg();
@@ -91,7 +91,7 @@ public class RuleTest extends TestCase {
     public void testLogCons2() {
         Agent ag = new Agent();
         ag.initAg();
-        
+
         Rule r = new Rule(Literal.parseLiteral("r([],a(X),b(X,4))"), Literal.parseLiteral("true"));
         ag.getBB().add(r);
 
@@ -104,28 +104,28 @@ public class RuleTest extends TestCase {
         assertEquals( ((Structure)u.get("X")).getArity(), 2);
         assertEquals( ((Structure)u.get("X")).getFunctor(), "b");
         assertEquals(result.capply(u), Literal.parseLiteral("r([],a(20),b(20,4))"));
-        
+
         iun = Literal.parseLiteral("r([],a(20),b(X,Y))").logicalConsequence(ag, new Unifier());
         u = iun.next();
         assertEquals(u.get("X"), ASSyntax.createNumber(20));
         assertEquals(u.get("Y"), ASSyntax.createNumber(4));
-    
-    }    
+
+    }
 
     public void testLogConsRec() {
         Agent ag = new Agent();
         ag.initAg();
-        
-        // add 
+
+        // add
         // min([],M,M).
         // min([C|T],V,M) :- C < V & min(T,C,M).
         // min([_|T],V,M) :- min(T,V,M).
         //ag.getBB().add(Literal.parseLiteral("min([],M,M)"));
         ag.getBB().add(new Rule(Literal.parseLiteral("min([],M,M)"), Literal.LTrue));
-        ag.getBB().add(new Rule(Literal.parseLiteral("min([op(C)|T], op(V), M)"), 
-                LogExpr.parseExpr("C < V & min(T,op(C),M)")));
-        ag.getBB().add(new Rule(Literal.parseLiteral("min([op(C)|T], op(V), M)"), 
-                LogExpr.parseExpr("C >= V & min(T,op(V),M)")));
+        ag.getBB().add(new Rule(Literal.parseLiteral("min([op(C)|T], op(V), M)"),
+                                LogExpr.parseExpr("C < V & min(T,op(C),M)")));
+        ag.getBB().add(new Rule(Literal.parseLiteral("min([op(C)|T], op(V), M)"),
+                                LogExpr.parseExpr("C >= V & min(T,op(V),M)")));
 
         Iterator<Unifier> iun = Literal.parseLiteral("min([],op(20),op(M))").logicalConsequence(ag, new Unifier());
         assertTrue(iun.hasNext());
@@ -135,27 +135,27 @@ public class RuleTest extends TestCase {
         Literal cons = Literal.parseLiteral("min([op(5),op(3),op(8),op(1),op(40)],op(1000),op(M))");
         Iterator<Literal> il = ag.getBB().getCandidateBeliefs(cons, null);
         assertEquals(3,iteratorSize(il));
-        
+
         iun = cons.logicalConsequence(ag, new Unifier());
         u = iun.next();
         assertEquals((int)Double.parseDouble(u.get("M").toString()),1);
-    }    
-    
+    }
+
     public void testHasVar() {
         Rule r = new Rule(Literal.parseLiteral("a(X,Y)"), LogExpr.parseExpr("b(X) & c(Y,W) & d(Y,W,R)"));
-        
+
         Map<VarTerm, Integer> c = new HashMap<VarTerm, Integer>();
         r.countVars(c);
         assertEquals(3,c.get(new VarTerm("Y")).intValue());
-        
+
         assertEquals(1, r.getSingletonVars().size());
         assertEquals("[R]", r.getSingletonVars().toString());
-        
+
         assertTrue(r.hasVar(new VarTerm("X"), null));
         assertTrue(r.hasVar(new VarTerm("W"), null));
-        assertFalse(r.hasVar(new VarTerm("III"), null));        
+        assertFalse(r.hasVar(new VarTerm("III"), null));
     }
-    
+
     public void testAsTerm() throws ParseException {
         Term t = ASSyntax.parseTerm("{p :- a & b | c}");
         assertEquals("{ p :- ((a & b) | c) }", t.toString());
@@ -166,22 +166,22 @@ public class RuleTest extends TestCase {
         t = ASSyntax.parsePlan("+!test4  <- +>{ +!g : v <- .print(ok) }; !test4a.");
         assertEquals("+!test4 <- .add_plan({ +!g : v <- .print(ok) },self,end); !test4a.", t.toString());
     }
-    
+
     public void testMaiquel() throws ParseException, RevisionFailedException {
         Agent ag = new Agent();
         ag.initAg();
         //first rule: rule1(X):-teste(X)&not(prop(X))&X
         ag.addBel(createRule(parseLiteral("rule1(X)"), parseFormula("teste(X)&not(prop(X))&X")));
-        
+
         //second rule: rule2(X):-teste(X)&X&not(prop(X))
         ag.addBel(createRule(parseLiteral("rule2(X)"), parseFormula("teste(X)&X&not(prop(X))")));
-        
+
         //asserting some facts
         //ag.addBel(parseLiteral("teste(p(K))")); // original proposal and the cause of the problem => test does not produce a ground literal
         ag.addBel(parseLiteral("teste(p(tom))")); // correction
         ag.addBel(parseLiteral("teste(p(bob))")); // correction
-        
-        
+
+
         ag.addBel(parseLiteral("p(tom)"));
         ag.addBel(parseLiteral("p(bob)"));
         /*
@@ -197,8 +197,8 @@ public class RuleTest extends TestCase {
             System.out.print(it2.next());
         }
         */
-        
-        //asserting a new fact          
+
+        //asserting a new fact
         ag.addBel(parseLiteral("prop(p(bob))"));
         assertEquals(1, iteratorSize(parseFormula("rule1(A)").logicalConsequence(ag, new Unifier())));
         assertEquals(1, iteratorSize(parseFormula("rule2(A)").logicalConsequence(ag, new Unifier())));
@@ -216,8 +216,8 @@ public class RuleTest extends TestCase {
         }
         */
     }
-    
-    
+
+
     @SuppressWarnings("unchecked")
     private int iteratorSize(Iterator i) {
         int c = 0;
@@ -227,5 +227,5 @@ public class RuleTest extends TestCase {
         }
         return c;
     }
-    
+
 }
