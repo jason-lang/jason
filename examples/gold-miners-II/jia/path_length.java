432f56f36b0aa1ff76a7f2c6ec581bbad7fbd5aa
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:42 -0300

    path_length.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- examples/gold-miners-II/jia/path_length.java
+++ examples/gold-miners-II/jia/path_length.java
@@ -15,26 +15,26 @@ import arch.MinerArch;
 import busca.Nodo;
 
 public class path_length extends DefaultInternalAction {
-    
+
     @Override
     public Object execute(TransitionSystem ts, Unifier un, Term[] terms) throws Exception {
         try {
             LocalWorldModel model = ((MinerArch)ts.getUserAgArch()).getModel();
-    
+
             int iagx = (int)((NumberTerm)terms[0]).solve();
             int iagy = (int)((NumberTerm)terms[1]).solve();
             int itox = (int)((NumberTerm)terms[2]).solve();
             int itoy = (int)((NumberTerm)terms[3]).solve();
-            
+
             if (model.inGrid(itox,itoy)) {
-                
+
                 // destination should be a free place
                 while (!model.isFreeOfObstacle(itox,itoy) && itox > 0) itox--;
                 while (!model.isFreeOfObstacle(itox,itoy) && itox < model.getWidth()) itox++;
 
                 Location from = new Location(iagx, iagy);
                 Location to   = new Location(itox, itoy);
-                
+
                 Nodo solution = new Search(model, from, to).search();
                 if (solution != null) {
                     int length = solution.getProfundidade();
