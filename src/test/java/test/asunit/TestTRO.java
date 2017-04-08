5e34ff146958c76b2abeedb13278b3413fb59071
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:49 -0300

    TestTRO.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/test/java/test/asunit/TestTRO.java
+++ src/test/java/test/asunit/TestTRO.java
@@ -8,24 +8,24 @@ import org.junit.Test;
 // test Tail Recursion Optimization
 public class TestTRO {
 
-    TestAgent ag; 
+    TestAgent ag;
 
     // initialisation of the agent test
     @Before
     public void setupAg() {
         ag = new TestAgent();
-        
+
         // defines the agent's AgentSpeak code
         ag.parseAScode(
-                "+!run <- !fat(5,1,F); jason.asunit.print(F). "+
-                "+!fat(1,V,V). "+
-                "+!fat(N,A,X) <- !fat(N-1,N*A,X)."
+            "+!run <- !fat(5,1,F); jason.asunit.print(F). "+
+            "+!fat(1,V,V). "+
+            "+!fat(N,A,X) <- !fat(N-1,N*A,X)."
         );
     }
-    
+
     @Test(timeout=2000)
     public void testGoalSrouce() {
-        ag.addGoal("run");      
+        ag.addGoal("run");
         ag.assertPrint("120", 50);
     }
 }
