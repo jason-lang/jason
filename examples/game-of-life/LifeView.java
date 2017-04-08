94fe9ec64a6a9418c68100d9cb60696a9331e31f
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:41 -0300

    LifeView.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- examples/game-of-life/LifeView.java
+++ examples/game-of-life/LifeView.java
@@ -4,20 +4,20 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.*;
 
-    
+
 /** class that implements the View of the Game of Life application */
 public class LifeView extends GridWorldView {
 
     private static final long serialVersionUID = 1L;
 
     LifeModel hmodel;
-    
+
     public LifeView(LifeModel model, final LifeEnvironment env) {
         super(model, "Game of Life", 500);
         hmodel = model;
         setVisible(true);
         repaint();
-        
+
         getCanvas().addMouseListener(new MouseListener() {
             public void mouseClicked(MouseEvent e) {
                 int col = e.getX() / cellSizeW;
@@ -39,11 +39,11 @@ public class LifeView extends GridWorldView {
     public void drawAgent(Graphics g, int x, int y, Color c, int id) {
         c = Color.white;
         if (hmodel.isAlive(x,y)) {
-            c = Color.darkGray;            
+            c = Color.darkGray;
         }
         g.setColor(c);
         g.fillRect(x * cellSizeW + 1, y * cellSizeH+1, cellSizeW-1, cellSizeH-1);
     }
-    
-    
+
+
 }
