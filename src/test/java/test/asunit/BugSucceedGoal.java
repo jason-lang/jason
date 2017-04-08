ed21a27adae81028d05693ea5b7568b7c09f7e68
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:48 -0300

    BugSucceedGoal.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/test/java/test/asunit/BugSucceedGoal.java
+++ src/test/java/test/asunit/BugSucceedGoal.java
@@ -7,26 +7,26 @@ import org.junit.Test;
 
 public class BugSucceedGoal {
 
-    TestAgent ag; 
+    TestAgent ag;
 
     // initialisation of the agent test
     @Before
     public void setupAg() {
         ag = new TestAgent();
-        
+
         // defines the agent's AgentSpeak code
         ag.parseAScode(
-                "!g. +!g <- !go; jason.asunit.print(end). "+ 
-                "+!go <- !go2; jason.asunit.print(a3). "+
-                "+!go2 <- .succeed_goal(go2); jason.asunit.print(a4). "
+            "!g. +!g <- !go; jason.asunit.print(end). "+
+            "+!go <- !go2; jason.asunit.print(a3). "+
+            "+!go2 <- .succeed_goal(go2); jason.asunit.print(a4). "
         );
     }
-    
-        
+
+
     @Test(timeout=2000)
     public void test1() {
         ag.addGoal("g");
-        ag.assertPrint("a3", 30); 
-        ag.assertPrint("end", 30); 
+        ag.assertPrint("a3", 30);
+        ag.assertPrint("end", 30);
     }
 }
