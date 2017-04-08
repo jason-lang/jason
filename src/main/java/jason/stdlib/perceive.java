828d9019e0104324009e37c2ab061b022c20fadf
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:47 -0300

    perceive.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/stdlib/perceive.java
+++ src/main/java/jason/stdlib/perceive.java
@@ -7,21 +7,25 @@ import jason.asSyntax.Term;
 
 /**
   <p>Internal action: <b><code>.perceive</code></b>.
-  
+
   <p>Description: forces the agent architecture to do perception of the
   environment immediately. It is normally used when the number of reasoning
   cycles before perception takes place was changed (this is normally at every
   cycle).
 
-  <p>Example:<ul> 
+  <p>Example:<ul>
 
   <li> <code>.perceive</code>.</li>
 
   </ul>
 */
 public class perceive extends DefaultInternalAction {
-    @Override public int getMinArgs() { return 0; }
-    @Override public int getMaxArgs() { return 0; }
+    @Override public int getMinArgs() {
+        return 0;
+    }
+    @Override public int getMaxArgs() {
+        return 0;
+    }
 
     @Override
     public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
