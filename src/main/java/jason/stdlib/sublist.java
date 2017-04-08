929f332e742678e3ca7a839bbe69fde7450607d6
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:48 -0300

    sublist.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/stdlib/sublist.java
+++ src/main/java/jason/stdlib/sublist.java
@@ -15,19 +15,19 @@ import java.util.List;
 /**
 
   <p>Internal action: <b><code>.sublist(<i>S</i>,<i>L</i>)</code></b>.
-  
+
   <p>Description: checks if some list <i>S</i> is a sublist of list <i>L</i>. If
   <i>S</i> has free variables, this internal action backtracks all
   possible values for <i>S</i>. This is based on .prefix and .suffix (try prefixes first then prefixes of each suffix).
 
   <p>Parameters:<ul>
-  
+
   <li>+/- sublist (list): the sublist to be checked.</li>
   <li>+ list (list): the list where the sublist is from.</li>
-  
+
   </ul>
 
-  <p>Examples:<ul> 
+  <p>Examples:<ul>
 
   <li> <code>.sublist([a],[a,b,c])</code>: true.</li>
   <li> <code>.sublist([b],[a,b,c])</code>: true.</li>
@@ -57,25 +57,29 @@ import java.util.List;
 
 */
 public class sublist extends DefaultInternalAction {
-    
+
     private static final long serialVersionUID = -1725808189703510112L;
     private static InternalAction singleton = null;
     public static InternalAction create() {
-        if (singleton == null) 
+        if (singleton == null)
             singleton = new sublist();
         return singleton;
     }
 
     // Needs exactly 2 arguments
-    @Override public int getMinArgs() { return 2; }
-    @Override public int getMaxArgs() { return 2; } 
+    @Override public int getMinArgs() {
+        return 2;
+    }
+    @Override public int getMaxArgs() {
+        return 2;
+    }
 
     // improve the check of the arguments to also check the type of the arguments
     @Override protected void checkArguments(Term[] args) throws JasonException {
         super.checkArguments(args); // check number of arguments
         if (!args[0].isList() && !args[0].isVar())
             throw JasonException.createWrongArgument(this,"first argument must be a list or a variable");
-        if (!args[1].isList()) 
+        if (!args[1].isList())
             throw JasonException.createWrongArgument(this,"second argument must be a list");
     }
 
@@ -83,17 +87,17 @@ public class sublist extends DefaultInternalAction {
     public Object execute(TransitionSystem ts, final Unifier un, final Term[] args) throws Exception {
         checkArguments(args);
         final Term sublist = args[0];
-        
+
         return new Iterator<Unifier>() {
             Unifier  c = null; // the current response (which is an unifier)
             ListTerm listOutter = ((ListTerm)args[1]);
-            List<Term> list = listOutter.getAsList(); // used in the inner loop, Java List is used for faster remove in the end 
+            List<Term> list = listOutter.getAsList(); // used in the inner loop, Java List is used for faster remove in the end
             boolean  triedEmpty = false;
-            
+
             public boolean hasNext() {
-                if (c == null) // the first call of hasNext should find the first response 
+                if (c == null) // the first call of hasNext should find the first response
                     find();
-                return c != null; 
+                return c != null;
             }
 
             public Unifier next() {
@@ -102,7 +106,7 @@ public class sublist extends DefaultInternalAction {
                 find(); // find next response
                 return b;
             }
-            
+
             void find() {
                 while (listOutter != null && !listOutter.isEmpty()) {
                     while (!list.isEmpty()) {
@@ -116,16 +120,16 @@ public class sublist extends DefaultInternalAction {
                     listOutter = listOutter.getNext();
                     if (listOutter == null || listOutter.isVar()) // the case of lists with tail
                         break;
-                    list = listOutter.getAsList(); 
+                    list = listOutter.getAsList();
                 }
                 if (!triedEmpty) {
                     triedEmpty = true;
                     c = un.clone();
                     if (c.unifiesNoUndo(sublist, ASSyntax.createList())) {
                         return; // found another sublist, c is the current response
-                    }                   
+                    }
                 }
-                c = null; // no more sublists found 
+                c = null; // no more sublists found
             }
 
             public void remove() {}
