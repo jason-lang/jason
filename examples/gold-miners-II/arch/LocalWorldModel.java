667143eb36c5c8c87618a3ca87b36b313864ec14
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:41 -0300

    LocalWorldModel.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- examples/gold-miners-II/arch/LocalWorldModel.java
+++ examples/gold-miners-II/arch/LocalWorldModel.java
@@ -9,25 +9,25 @@ import env.WorldModel;
 
 /**
  * Class used to model the scenario (for an agent local view)
- * 
+ *
  * @author jomi
  */
 public class LocalWorldModel extends WorldModel {
 
     int[][]                   visited; // count the visited locations
     int                       minVisited = 0; // min value for near least visited
-    
+
     private Random            random = new Random();
-    
+
     //private Logger            logger   = Logger.getLogger("jasonTeamSimLocal.mas2j." + LocalWorldModel.class.getName());
 
     public static LocalWorldModel create(int w, int h, int nbAg) {
         return new LocalWorldModel(w,h,nbAg);
     }
-    
+
     public LocalWorldModel(int w, int h, int nbAg) {
         super(w, h, nbAg);
-        
+
         visited = new int[getWidth()][getHeight()];
         for (int i = 0; i < getWidth(); i++) {
             for (int j = 0; j < getHeight(); j++) {
@@ -36,7 +36,7 @@ public class LocalWorldModel extends WorldModel {
         }
     }
 
-    @Override 
+    @Override
     public boolean isFree(int x, int y) {
         return super.isFree(x,y) && !hasObject(ENEMY, x, y);
     }
@@ -48,7 +48,7 @@ public class LocalWorldModel extends WorldModel {
         visited[x][y] = 10000;
         super.setDepot(x, y);
     }
-    
+
     public int getVisited(Location l) {
         return visited[l.x][l.y];
     }
@@ -58,7 +58,7 @@ public class LocalWorldModel extends WorldModel {
     }
     public void incVisited(int x, int y) {
         visited[x][y] += 2;
-        
+
         if (x > 0) visited[x-1][y]++;
         if (y > 0) visited[x][y-1]++;
         if (y > 0 && x > 0) visited[x-1][y-1]++;
@@ -68,12 +68,12 @@ public class LocalWorldModel extends WorldModel {
         if (x+1 < getWidth() && y > 0) visited[x+1][y-1]++;
         if (x+1 < getWidth() && y+1 < getHeight()) visited[x+1][y+1]++;
     }
-    
+
     /** returns the near location of x,y that was least visited */
     public Location getNearLeastVisited(int agx, int agy) {
-        //int distanceToBorder = (agx < getWidth()/2 ? agx : getWidth() - agx) - 1; 
+        //int distanceToBorder = (agx < getWidth()/2 ? agx : getWidth() - agx) - 1;
         Location agloc = new Location(agx,agy);
-        
+
         /*
         logger.info("------");
         for (int i = 0; i < getWidth(); i++) {
@@ -84,54 +84,59 @@ public class LocalWorldModel extends WorldModel {
             logger.info(line);
         }
         */
-        
+
         //int visitedTarget = 0;
         while (true) {
 
             int x = agx;
             int y = agy;
-            int w = 1; 
+            int w = 1;
             int dx = 0;
             int dy = 0;
             int stage = 1;//(x % 2 == 0 ? 1 : 2);
             Location better = null;
-            
+
             while (w < getWidth()) { //( (w/2+distanceToBorder) < getWidth()) {
                 switch (stage) {
-                    case 1: if (dx < w) {
-                                dx++;
-                                break;
-                            } else {
-                                stage = 2;//(x % 2 == 0) ? 2 : 3; 
-                            }
-                    case 2: if (dy < w) {
-                                dy++;
-                                break;
-                            } else {
-                                stage = 3;//(x % 2 == 0) ? 3 : 1;
-                            }
-                    case 3: if (dx > 0) {
-                                dx--;
-                                break;
-                            } else {
-                                stage = 4;
-                            }
-                    case 4: if (dy > 0) {
-                                dy--;
-                                break;
-                            } else {
-                                stage = 1;
-                                x--;
-                                y--;
-                                w += 2;
-                            }
+                case 1:
+                    if (dx < w) {
+                        dx++;
+                        break;
+                    } else {
+                        stage = 2;//(x % 2 == 0) ? 2 : 3;
+                    }
+                case 2:
+                    if (dy < w) {
+                        dy++;
+                        break;
+                    } else {
+                        stage = 3;//(x % 2 == 0) ? 3 : 1;
+                    }
+                case 3:
+                    if (dx > 0) {
+                        dx--;
+                        break;
+                    } else {
+                        stage = 4;
+                    }
+                case 4:
+                    if (dy > 0) {
+                        dy--;
+                        break;
+                    } else {
+                        stage = 1;
+                        x--;
+                        y--;
+                        w += 2;
+                    }
                 }
-                
+
                 Location l = new Location(x+dx,y+dy);
                 if (isFree(l) && !l.equals(agloc)) {
                     if (visited[l.x][l.y] < minVisited) { // a place better then minVisited! go there
                         return l;
-                    } if (visited[l.x][l.y] == minVisited) { // a place in the minVisited level
+                    }
+                    if (visited[l.x][l.y] == minVisited) { // a place in the minVisited level
                         if (better == null) {
                             better = l;
                         } else if (l.distance(agloc) < better.distance(agloc)) {
@@ -142,14 +147,14 @@ public class LocalWorldModel extends WorldModel {
                     }
                 }
             } // end while
-            
+
             if (better != null) {
                 return better;
             }
             minVisited++;
         }
     }
-    
+
 
     /** removes enemies/gold around l */
     public void clearAgView(Location l) {
@@ -159,45 +164,45 @@ public class LocalWorldModel extends WorldModel {
     /** removes enemies/gold around x,y */
     public void clearAgView(int x, int y) {
         int e1 = ~(ENEMY + GOLD);
-        
+
         // nw
         if (x > 0 && y > 0) {
             data[x - 1][y - 1] &= e1;
             if (view != null) view.update(x-1,y-1);
-        } 
+        }
         // n
         if (y > 0) {
             data[x][y - 1] &= e1;
             if (view != null) view.update(x,y-1);
-        } 
+        }
         // ne
         if (x < (getWidth() - 1) && y > 0) {
             data[x + 1][y - 1] &= e1;
             if (view != null) view.update(x+1,y-1);
-        } 
+        }
         // w
         if (x > 0) {
             data[x - 1][y] &= e1;
             if (view != null) view.update(x-1,y);
-        } 
+        }
         // cur
         data[x][y] &= e1;
-        
+
         // e
         if (x < (getWidth() - 1)) {
             data[x + 1][y] &= e1;
             if (view != null) view.update(x+1,y);
-        } 
+        }
         // sw
         if (x > 0 && y < (getHeight() - 1)) {
             data[x - 1][y + 1] &= e1;
             if (view != null) view.update(x-1,y+1);
-        } 
+        }
         // s
         if (y < (getHeight() - 1)) {
             data[x][y + 1] &= e1;
             if (view != null) view.update(x,y+1);
-        } 
+        }
         // se
         if (x < (getWidth() - 1) && y < (getHeight() - 1)) {
             data[x + 1][y + 1] &= e1;
