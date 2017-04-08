d9e8e9ad773e848ef5a53b8039299dd697978c20
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:45 -0300

    sin.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/functions/sin.java
+++ src/main/java/jason/functions/sin.java
@@ -6,18 +6,18 @@ import jason.asSemantics.TransitionSystem;
 import jason.asSyntax.NumberTerm;
 import jason.asSyntax.Term;
 
-/** 
+/**
   <p>Function: <b><code>math.sin(N)</code></b>: encapsulates java Math.sin(N),
   returns the trigonometric sine of an angle.
-  
-  @author Jomi 
+
+  @author Jomi
 */
 public class sin extends DefaultArithFunction  {
 
     public String getName() {
         return "math.sin";
     }
-    
+
     @Override
     public double evaluate(TransitionSystem ts, Term[] args) throws Exception {
         if (args[0].isNumeric()) {
@@ -31,5 +31,5 @@ public class sin extends DefaultArithFunction  {
     public boolean checkArity(int a) {
         return a == 1;
     }
-    
+
 }
