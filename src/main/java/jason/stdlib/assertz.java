37a130aa7514e2e28dbc63fd55ee3c0c7698298e
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:46 -0300

    assertz.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/stdlib/assertz.java
+++ src/main/java/jason/stdlib/assertz.java
@@ -11,25 +11,29 @@ import java.util.List;
 
 /**
   <p>Internal action: <b><code>.assertz</code></b>.
-  
+
   <p>Description: adds a new belief as the "+>" operator. However, it can be used in prolog like rules.
-  
+
   <p>Parameters:<ul>
   <li>+ belief (literal): the belief that will be added in the end of the belief base.<br/>
   </ul>
-  
+
   <p>Examples:<ul>
   <li> <code>.assertz(p)</code>: adds <code>p</code> in the belief base.</li>
   </ul>
-  
+
   @see jason.stdlib.asserta
   @see jason.stdlib.abolish
 
  */
 public class assertz extends DefaultInternalAction {
 
-    @Override public int getMinArgs() { return 1; }
-    @Override public int getMaxArgs() { return 1; }
+    @Override public int getMinArgs() {
+        return 1;
+    }
+    @Override public int getMaxArgs() {
+        return 1;
+    }
 
     @Override protected void checkArguments(Term[] args) throws JasonException {
         super.checkArguments(args); // check number of arguments
@@ -37,16 +41,16 @@ public class assertz extends DefaultInternalAction {
             if (!args[0].isGround() && !args[0].isRule())
                 throw JasonException.createWrongArgument(this,"first argument must be a ground literal (or rule).");
     }
-    
+
     @Override public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
         checkArguments(args);
 
-        List<Literal>[] result = ts.getAg().brf((Literal)args[0],null,null,true); 
+        List<Literal>[] result = ts.getAg().brf((Literal)args[0],null,null,true);
         if (result != null) { // really added something
             // generate events
             ts.updateEvents(result,null);
         }
         return true;
     }
-    
+
 }
