6184d7054e706d58c3d7707627f2e1976f88bd31
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:48 -0300

    TestCopyTerm.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/test/java/test/asunit/TestCopyTerm.java
+++ src/test/java/test/asunit/TestCopyTerm.java
@@ -9,17 +9,17 @@ public class TestCopyTerm {
     @Test(timeout=2000)
     public void testUnnamedVar() {
         TestAgent ag = new TestAgent();
-        
+
         // defines the agent's AgentSpeak code
         ag.parseAScode(
-                "copy_term(T,T). "+
-                "+!test1 : copy_term(a(B),Y) <- jason.asunit.print(Y). "+
-                "+!test2 : copy_term(a(B),Y) <- B=10; jason.asunit.print(Y). "
+            "copy_term(T,T). "+
+            "+!test1 : copy_term(a(B),Y) <- jason.asunit.print(Y). "+
+            "+!test2 : copy_term(a(B),Y) <- B=10; jason.asunit.print(Y). "
         );
-        ag.addGoal("test1");      
-        ag.assertPrint("a(_", 10); // cannot print(a(3)        
-        ag.addGoal("test2");      
-        ag.assertPrint("a(10)", 10); // cannot print(a(3)        
+        ag.addGoal("test1");
+        ag.assertPrint("a(_", 10); // cannot print(a(3)
+        ag.addGoal("test2");
+        ag.assertPrint("a(10)", 10); // cannot print(a(3)
     }
 
 }
