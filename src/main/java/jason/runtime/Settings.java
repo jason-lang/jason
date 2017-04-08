1255ee7d363b4c332c988c3a988333634ee46463
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:46 -0300

    Settings.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/runtime/Settings.java
+++ src/main/java/jason/runtime/Settings.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
 
 /** MAS Runtime Settings for an Agent (from mas2j file, agent declaration) */
 public class Settings {
-    
+
     public static final byte      ODiscard        = 1;
     public static final byte      ORequeue        = 2;
     public static final byte      ORetrieve       = 3;
@@ -18,31 +18,31 @@ public class Settings {
     public static final int       ODefaultNRC     = 1;
     public static final int       ODefaultVerbose = -1;
     public static final boolean   ODefaultSync    = false;
-    
-    private static Logger logger = Logger.getLogger(Settings.class.getName());          
-        
+
+    private static Logger logger = Logger.getLogger(Settings.class.getName());
+
     private byte    events     = ODiscard;
     private boolean intBels    = OSameFocus;
     private int     nrcbp      = ODefaultNRC;
     private int     verbose    = ODefaultVerbose;
-    private boolean sync       = ODefaultSync; 
+    private boolean sync       = ODefaultSync;
     private boolean qCache     = false; // whether to use query cache
     private boolean qProfiling = false; // whether has query profiling
     private boolean troON      = true;  // tail recursion optimisation is on by default
-    
+
     private Map<String,Object> userParameters = new HashMap<String,Object>();
-    
+
     public static final String PROJECT_PARAMETER = "project-parameter";
     public static final String INIT_BELS  = "beliefs";
-    public static final String INIT_GOALS = "goals"; 
+    public static final String INIT_GOALS = "goals";
     public static final String MIND_INSPECTOR = "mindinspector";
     public Settings() {
     }
-    
+
     public Settings(String options) {
         setOptions(options);
     }
-    
+
     @SuppressWarnings("unchecked")
     public void setOptions(String options) {
         logger.fine("Setting options from "+options);
@@ -54,13 +54,13 @@ public class Settings {
             logger.log(Level.SEVERE, "Error parsing options "+options,e);
         }
     }
-    
+
     public void setOptions(Map<String,Object> options) {
         if (options == null) return;
         userParameters = options;
-        
+
         for (String key: options.keySet()) {
-            
+
             if (key.equals("events")) {
                 String events = (String)options.get("events");
                 if (events.equals("discard")) {
@@ -82,11 +82,11 @@ public class Settings {
             } else if (key.equals("nrcbp")) {
                 String nrc = (String)options.get("nrcbp");
                 setNRCBP(nrc);
-    
+
             } else if (key.equals("verbose")) {
                 String verbose = (String)options.get("verbose");
                 setVerbose(verbose);
-    
+
             } else if (key.equals("synchronised")) {
                 setSync("true".equals((String)options.get("synchronised")));
             } else if (key.equals("tro")) {
@@ -137,7 +137,7 @@ public class Settings {
     public void setVerbose(int opt) {
         verbose = opt;
     }
-    
+
     public boolean discard() {
         return events == ODiscard;
     }
@@ -145,7 +145,7 @@ public class Settings {
     public boolean requeue() {
         return events == ORequeue;
     }
-    
+
     public boolean retrieve() {
         return events == ORetrieve;
     }
@@ -160,65 +160,68 @@ public class Settings {
     public int nrcbp() {
         return nrcbp;
     }
-    
+
     public int verbose() {
         return verbose;
     }
-    
+
     public java.util.logging.Level logLevel() {
-         switch(verbose) {
-         case 0 : return java.util.logging.Level.WARNING;
-         case 1 : return java.util.logging.Level.INFO;
-         case 2 : return java.util.logging.Level.FINE;
-         }
-         return java.util.logging.Level.INFO;
-   }
-    
+        switch(verbose) {
+        case 0 :
+            return java.util.logging.Level.WARNING;
+        case 1 :
+            return java.util.logging.Level.INFO;
+        case 2 :
+            return java.util.logging.Level.FINE;
+        }
+        return java.util.logging.Level.INFO;
+    }
+
     /** returns true if the execution is synchronised */
     public boolean isSync() {
         return sync;
     }
-    
+
     public void setSync(boolean pSync) {
         sync = pSync;
     }
-    
+
     public boolean isTROon() {
         return troON;
     }
     public void setTRO(boolean tro) {
         troON = tro;
     }
-    
+
     public boolean hasQueryCache() {
         return qCache;
     }
     public void setQueryCache(boolean b) {
         qCache = b;
     }
-    
+
     public boolean hasQueryProfiling() {
         return qProfiling;
     }
     public void setQueryProfiling(boolean b) {
         qProfiling = b;
     }
-    
+
     public Map<String,Object> getUserParameters() {
         return userParameters;
     }
-    
+
     public String getUserParameter(String key) {
         String vl = (String)userParameters.get(key);
         if (vl != null && vl.startsWith("\"") && vl.endsWith("\"")) {
             vl = vl.substring(1, vl.length()-1);
-            vl = vl.replaceAll("\\\\\"", "\"");        
+            vl = vl.replaceAll("\\\\\"", "\"");
         }
         return vl;
     }
-    
+
     public Object removeUserParameter(String key) {
         return userParameters.remove(key);
     }
-    
+
 }
