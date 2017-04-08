82adc5ddb8a06fd7330d9e4f921e77a14f34e7e0
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:42 -0300

    RoomEnv.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- examples/room/RoomEnv.java
+++ examples/room/RoomEnv.java
@@ -3,17 +3,17 @@ import jason.asSyntax.Structure;
 import jason.environment.Environment;
 
 public class RoomEnv extends Environment {
-    
+
     Literal ld  = Literal.parseLiteral("locked(door)");
     Literal nld = Literal.parseLiteral("~locked(door)");
     boolean doorLocked = true;
-    
+
     @Override
     public void init(String[] args) {
         // initial percepts
         addPercept(ld);
     }
-    
+
     /**
      * Implementation of the agent's basic actions
      */
@@ -21,13 +21,13 @@ public class RoomEnv extends Environment {
     public boolean executeAction(String ag, Structure act) {
         System.out.println("Agent "+ag+" is doing "+act);
         clearPercepts();
-        
+
         if (act.getFunctor().equals("lock"))
             doorLocked = true;
 
         if (act.getFunctor().equals("unlock"))
             doorLocked = false;
-    
+
         // update percepts given state of the environment
         if (doorLocked)
             addPercept(ld);
