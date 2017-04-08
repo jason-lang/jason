4f094cdbe9eae3149de45f86e2b43c9de8551491
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:49 -0300

    TestGoalSource.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/test/java/test/asunit/TestGoalSource.java
+++ src/test/java/test/asunit/TestGoalSource.java
@@ -7,23 +7,23 @@ import org.junit.Test;
 
 public class TestGoalSource {
 
-    TestAgent ag; 
+    TestAgent ag;
 
     // initialisation of the agent test
     @Before
     public void setupAg() {
         ag = new TestAgent();
-        
+
         // defines the agent's AgentSpeak code
         ag.parseAScode(
-                "+!begin <- !g; !g[source(bob)]. "+
-                "+!g[source(A)] <- jason.asunit.print(A)."
+            "+!begin <- !g; !g[source(bob)]. "+
+            "+!g[source(A)] <- jason.asunit.print(A)."
         );
     }
-    
+
     @Test(timeout=2000)
     public void testGoalSrouce() {
-        ag.addGoal("begin");      
+        ag.addGoal("begin");
         ag.assertPrint("self", 5);
         ag.assertPrint("bob", 5);
     }
