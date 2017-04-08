d5eac50d029a3ff67c742ec10e61d1863a510a6b
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:48 -0300

    BugVarsInInitBels.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/test/java/test/asunit/BugVarsInInitBels.java
+++ src/test/java/test/asunit/BugVarsInInitBels.java
@@ -8,26 +8,26 @@ import org.junit.Test;
 /** based on bug found by Neil Madden -- see jason list */
 public class BugVarsInInitBels {
 
-    TestAgent ag; 
+    TestAgent ag;
 
     // initialisation of the agent test
     @Before
     public void setupAg() {
         ag = new TestAgent();
-        
+
         // defines the agent's AgentSpeak code
         ag.parseAScode(
-                "test_rule(A,A). "+ // do not replace A by _ (the test is just the name A for the var)
-                "!test. "+
-                "+!test <- A = test_wrong_value; "+
-                "          ?test_rule(T,right_value); "+
-                "          act(T)." // T should be 'right value'
+            "test_rule(A,A). "+ // do not replace A by _ (the test is just the name A for the var)
+            "!test. "+
+            "+!test <- A = test_wrong_value; "+
+            "          ?test_rule(T,right_value); "+
+            "          act(T)." // T should be 'right value'
         );
     }
-    
+
     @Test(timeout=2000)
     public void testRule() {
-        ag.assertAct("act(right_value)", 10); 
+        ag.assertAct("act(right_value)", 10);
     }
 
 }
