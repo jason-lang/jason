ccab496d39ec9c1ef07c9f36fe1d5e1b01748e29
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:42 -0300

    Resource.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- examples/mining-robots/env/Resource.java
+++ examples/mining-robots/env/Resource.java
@@ -6,35 +6,35 @@ public class Resource extends PlanetCell {
     private int amount;
 
     public Resource(int t, int am) {
-        
+
         type = t;
         amount = am;
-    
+
     }
-    
+
     public int getType() {
-    
+
         return type;
-    
+
     }
-    
+
     public int getAmount() {
-    
+
         return amount;
-    
+
     }
-    
+
     public void mine() {
-    
+
         amount--;
-    
+
     }
-    
+
     public boolean depleted() {
-    
+
         return amount < 1;
-    
+
     }
-    
+
 }
 
