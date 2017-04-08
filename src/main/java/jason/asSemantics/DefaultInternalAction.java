7ea5e2c50b9c28c8614c25183a66367321e236a8
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:43 -0300

    DefaultInternalAction.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/asSemantics/DefaultInternalAction.java
+++ src/main/java/jason/asSemantics/DefaultInternalAction.java
@@ -7,28 +7,36 @@ import jason.asSyntax.Term;
 import java.io.Serializable;
 
 /**
- * Default implementation of the internal action interface (it simply returns false 
+ * Default implementation of the internal action interface (it simply returns false
  * for the interface methods).
- * 
+ *
  * Useful to create new internal actions.
- * 
+ *
  * @author Jomi
  */
 public class DefaultInternalAction implements InternalAction, Serializable {
-    
+
     private static final long serialVersionUID = 1L;
 
-    public boolean suspendIntention()   { return false;  }
-    public boolean canBeUsedInContext() { return true;  }
+    public boolean suspendIntention()   {
+        return false;
+    }
+    public boolean canBeUsedInContext() {
+        return true;
+    }
 
-    public int getMinArgs() { return 0; }
-    public int getMaxArgs() { return Integer.MAX_VALUE; }
-    
+    public int getMinArgs() {
+        return 0;
+    }
+    public int getMaxArgs() {
+        return Integer.MAX_VALUE;
+    }
+
     protected void checkArguments(Term[] args) throws JasonException {
         if (args.length < getMinArgs() || args.length > getMaxArgs())
-            throw JasonException.createWrongArgumentNb(this);            
+            throw JasonException.createWrongArgumentNb(this);
     }
-    
+
     public Term[] prepareArguments(Literal body, Unifier un) {
         Term[] terms = new Term[body.getArity()];
         for (int i=0; i<terms.length; i++) {
@@ -36,12 +44,12 @@ public class DefaultInternalAction implements InternalAction, Serializable {
         }
         return terms;
     }
-    
+
     public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
         return false;
     }
-    
+
     public void destroy() throws Exception {
-        
+
     }
 }
