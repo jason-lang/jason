a164f1e66e474a187d470b9600ed2e7f8d749fc1
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:42 -0300

    get_direction.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- examples/gold-miners/jia/get_direction.java
+++ examples/gold-miners/jia/get_direction.java
@@ -20,28 +20,28 @@ import busca.Heuristica;
 import busca.Nodo;
 
 public class get_direction extends DefaultInternalAction {
-    
+
     @Override
     public Object execute(TransitionSystem ts, Unifier un, Term[] terms) throws Exception {
         try {
             String sAction = "skip";
 
             WorldModel model = WorldModel.get();
-    
+
             int iagx = (int)((NumberTerm)terms[0]).solve();
             int iagy = (int)((NumberTerm)terms[1]).solve();
             int itox = (int)((NumberTerm)terms[2]).solve();
             int itoy = (int)((NumberTerm)terms[3]).solve();
-            
+
             if (model.inGrid(itox,itoy)) {
                 Busca searchAlg = new AEstrela();
                 //searchAlg.setMaxAbertos(1000);
                 Location lini = new Location(iagx, iagy);
-                
+
                 // destination should be a free place
                 while (!model.isFreeOfObstacle(itox,itoy) && itox > 0) itox--;
                 while (!model.isFreeOfObstacle(itox,itoy) && itox < model.getWidth()) itox++;
-                
+
                 Nodo solution = searchAlg.busca(new GridState(lini, lini,new Location(itox, itoy), model, "initial"));
                 if (solution != null) {
                     //System.out.println(iagx+"-"+iagy+"/"+itox+"-"+itoy+" = "+solution.montaCaminho());
@@ -75,7 +75,7 @@ class GridState implements Estado, Heuristica {
     Location from,to;
     String op;
     GridWorldModel model;
-    
+
     public GridState(Location l, Location from, Location to, GridWorldModel model, String op) {
         this.pos = l;
         this.from = from;
@@ -83,7 +83,7 @@ class GridState implements Estado, Heuristica {
         this.model = model;
         this.op = op;
     }
-    
+
     public int custo() {
         return 1;
     }
@@ -109,13 +109,13 @@ class GridState implements Estado, Heuristica {
         suc(s,new Location(pos.x,pos.y+1),"down");
         return s;
     }
-    
+
     private void suc(List<Estado> s, Location newl, String op) {
         if (model.isFree(newl) || (from.distance(newl) > 3 && model.isFreeOfObstacle(newl))) {
             s.add(new GridState(newl,from,to,model,op));
         }
     }
-    
+
     public boolean equals(Object o) {
         try {
             GridState m = (GridState)o;
@@ -123,12 +123,12 @@ class GridState implements Estado, Heuristica {
         } catch (Exception e) {}
         return false;
     }
-    
+
     public int hashCode() {
         return pos.hashCode();
     }
-            
+
     public String toString() {
-        return "(" + pos + "-" + op + ")"; 
+        return "(" + pos + "-" + op + ")";
     }
 }
