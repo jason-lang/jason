1127aaa050f6220962632d83ae6bd24f2c86aa93
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:47 -0300

    plan_label.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/stdlib/plan_label.java
+++ src/main/java/jason/stdlib/plan_label.java
@@ -10,21 +10,21 @@ import jason.asSyntax.Term;
 
 /**
   <p>Internal action: <b><code>.plan_label(<i>P</i>,<i>L</i>)</code></b>.
-  
+
   <p>Description: unifies <i>P</i> with a <i>plan term</i> representing the plan
   labeled with the term <i>L</i> within the agent's plan library.
-  
+
   <p>Parameters:<ul>
-  
+
   <li>- plan (plan term): the term representing the plan, it is
-  a plan enclosed by { and } 
+  a plan enclosed by { and }
   (e.g. <code>{+!g : vl(X) <- .print(X)}</code>).<br/>
-  
+
   <li>+ label (structure): the label of that plan.<br/>
-  
+
   </ul>
-  
-  <p>Example:<ul> 
+
+  <p>Example:<ul>
 
   <li> <code>.plan_label(P,p1)</code>: unifies P with the term
   representation of the plan labeled <code>p1</code>.</li>
@@ -37,16 +37,20 @@ import jason.asSyntax.Term;
 
  */
 public class plan_label extends DefaultInternalAction {
-	
-	private static InternalAction singleton = null;
-	public static InternalAction create() {
-		if (singleton == null) 
-			singleton = new plan_label();
-		return singleton;
-	}
 
-	@Override public int getMinArgs() { return 2; }
-    @Override public int getMaxArgs() { return 2; }
+    private static InternalAction singleton = null;
+    public static InternalAction create() {
+        if (singleton == null)
+            singleton = new plan_label();
+        return singleton;
+    }
+
+    @Override public int getMinArgs() {
+        return 2;
+    }
+    @Override public int getMaxArgs() {
+        return 2;
+    }
 
     @Override
     public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
@@ -58,9 +62,9 @@ public class plan_label extends DefaultInternalAction {
             p = ts.getAg().getPL().get( (Atom)label);
         else
             p = ts.getAg().getPL().get( new Atom(label.toString()));
-            
+
         if (p != null) {
-        	p = (Plan)p.clone();
+            p = (Plan)p.clone();
             p.getLabel().delSources();
             p.setAsPlanTerm(true);
             p.makeVarsAnnon();
@@ -68,7 +72,7 @@ public class plan_label extends DefaultInternalAction {
             //return un.unifies(new StringTermImpl(ps), args[0]);
             return un.unifies(p, args[0]);
         } else {
-        	return false;
+            return false;
         }
     }
 }
