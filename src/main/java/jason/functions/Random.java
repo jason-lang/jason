b062442b32d2a586aca3bbacf0e618b59a9b06e8
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:44 -0300

    Random.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/functions/Random.java
+++ src/main/java/jason/functions/Random.java
@@ -5,28 +5,28 @@ import jason.asSemantics.TransitionSystem;
 import jason.asSyntax.NumberTerm;
 import jason.asSyntax.Term;
 
-/** 
+/**
    <p>Function: <b><code>math.random(N)</code></b>: encapsulates java Math.random;
    If N is not informed: returns a value greater than or equal to 0.0 and less than 1.0;
    If N is informed: returns a value greater than or equal to 0.0 and less than N.
-  
+
    <p>Examples:<ul>
    <li> <code>math.random</code>: returns the random number between 0 and 1.</li>
    <li> <code>math.random(10)</code>: returns the random number between 0 and 9.9999.</li>
    </ul>
-   
+
    @author Jomi
-  
+
    @see jason.functions.floor
    @see jason.functions.ceil
-   
+
 */
 public class Random extends DefaultArithFunction  {
 
     public String getName() {
         return "math.random";
     }
-    
+
     @Override
     public double evaluate(TransitionSystem ts, Term[] args) throws Exception {
         if (args.length == 1 && args[0].isNumeric()) {
@@ -40,5 +40,5 @@ public class Random extends DefaultArithFunction  {
     public boolean checkArity(int a) {
         return a == 0 || a == 1;
     }
-    
+
 }
