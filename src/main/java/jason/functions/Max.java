5e4c94b8f0d2278040b2f8dc73b8ce43cfd28178
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:44 -0300

    Max.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/functions/Max.java
+++ src/main/java/jason/functions/Max.java
@@ -7,7 +7,7 @@ import jason.asSyntax.ListTerm;
 import jason.asSyntax.NumberTerm;
 import jason.asSyntax.Term;
 
-/** 
+/**
 <p>Function: <b><code>math.max(N1,N2)</code></b>: encapsulates java Math.max(N1,N2).
    It also returns the max number of a list.
 
@@ -15,8 +15,8 @@ import jason.asSyntax.Term;
 <li> <code>math.max(1,3)</code>: returns 3.</li>
 <li> <code>math.max([3,a,"s",5])</code>: returns 5.</li>
 </ul>
- 
-@author Jomi 
+
+@author Jomi
 
 @see jason.functions.Min
 
@@ -26,7 +26,7 @@ public class Max extends DefaultArithFunction  {
     public String getName() {
         return "math.max";
     }
-    
+
     @Override
     public double evaluate(TransitionSystem ts, Term[] args) throws Exception {
         if (args[0].isNumeric() && args[1].isNumeric()) {
