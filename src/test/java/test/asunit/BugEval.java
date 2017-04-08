8c74c9b96aca81c44ca9e4339d31f7fab2cc442e
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:48 -0300

    BugEval.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/test/java/test/asunit/BugEval.java
+++ src/test/java/test/asunit/BugEval.java
@@ -8,21 +8,21 @@ import org.junit.Test;
 /** based on bug found by Grimaldo */
 public class BugEval {
 
-    TestAgent ag; 
+    TestAgent ag;
 
     // initialisation of the agent test
     @Before
     public void setupAg() {
         ag = new TestAgent();
-        
+
         // defines the agent's AgentSpeak code
         ag.parseAScode(
-                "+!start  <- A1 = false;     B1 = true;     .eval(X1, A1 | B1);     jason.asunit.print(X1); "+
-                "            !a2(A2);     !b2(B2);   .eval(X2, A2 | B2);     jason.asunit.print(X2). "+
-                "+!a2(false). +!b2(true). "
+            "+!start  <- A1 = false;     B1 = true;     .eval(X1, A1 | B1);     jason.asunit.print(X1); "+
+            "            !a2(A2);     !b2(B2);   .eval(X2, A2 | B2);     jason.asunit.print(X2). "+
+            "+!a2(false). +!b2(true). "
         );
     }
-    
+
     @Test(timeout=2000)
     public void test() {
         ag.addGoal("start");
