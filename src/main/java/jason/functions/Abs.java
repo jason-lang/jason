60710cf8953e8251af0f0d16c41c6a159d69abc2
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:44 -0300

    Abs.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/functions/Abs.java
+++ src/main/java/jason/functions/Abs.java
@@ -6,22 +6,22 @@ import jason.asSemantics.TransitionSystem;
 import jason.asSyntax.NumberTerm;
 import jason.asSyntax.Term;
 
-/** 
+/**
   <p>Function: <b><code>math.abs(N)</code></b>: encapsulates java Math.abs(N).
-  
+
   <p>Examples:<ul>
   <li> <code>math.abs(1)</code>: returns 1.</li>
   <li> <code>math.abs(-1)</code>: returns 1.</li>
   </ul>
-   
-  @author Jomi 
+
+  @author Jomi
 */
 public class Abs extends DefaultArithFunction  {
 
     public String getName() {
         return "math.abs";
     }
-    
+
     @Override
     public double evaluate(TransitionSystem ts, Term[] args) throws Exception {
         if (args[0].isNumeric()) {
@@ -36,5 +36,5 @@ public class Abs extends DefaultArithFunction  {
     public boolean checkArity(int a) {
         return a == 1;
     }
-    
+
 }
