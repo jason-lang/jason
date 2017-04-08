1b33d6aa44395f8919ab555d77fc607676b56058
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:44 -0300

    Directive.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/asSyntax/directives/Directive.java
+++ src/main/java/jason/asSyntax/directives/Directive.java
@@ -4,18 +4,18 @@ import jason.asSemantics.Agent;
 import jason.asSyntax.Pred;
 import jason.asSyntax.parser.as2j;
 
-/** 
+/**
  * Interface for all compiler directives (e.g. include and goal patterns).
- * 
- * <p>There are two kinds of directives: single directive and begin/end directive. 
- * The single directive does not have inner plans, as, for instance, the 
+ *
+ * <p>There are two kinds of directives: single directive and begin/end directive.
+ * The single directive does not have inner plans, as, for instance, the
  * include:
  * <br/><code>
  *     ...<br>
  *     { include("a.asl") } <br>
  *     ...<br>
  * </code><br/>
- * 
+ *
  * <p>begin/end directives have inner plans, as used in goal patterns:
  * <br/><code>
  *     ...<br>
@@ -33,36 +33,36 @@ import jason.asSyntax.parser.as2j;
  *     -!g <- !g. <br>
  *      +g <- -p__1(g); -p__2(g); .dropGoal(g,true). <br>
  * </code><br/>
- * 
- * Goal patterns are proposed in the paper: <br> 
- * <br/> 
+ *
+ * Goal patterns are proposed in the paper: <br>
+ * <br/>
  * Jomi Fred Hubner, Rafael H. Bordini, and Michael Wooldridge. <br>
  * Programming declarative goals using plan patterns. <br> In Matteo
  * Baldoni and Ulle Endriss, editors, <i>Proceedings of the Fourth
  * International Workshop on Declarative Agent Languages and
  * Technologies</i> (DALT 2006), held with AAMAS 2006, 8th May, Hakodate,
- * Japan, pages 65-81. Springer, 2006. <br> 
+ * Japan, pages 65-81. Springer, 2006. <br>
  * <br/>
- * 
+ *
  * @author jomi
  */
 public interface Directive {
     /** called when the directive is declared */
     void begin(Pred directive, as2j parser);
-    
+
     /**
      * This method is called to process the directive.
-     * 
+     *
      * @param directive the directive as defined in the source (e.g. "include("bla.asl")")
      * @param outerContent the representation of the agent where the directive is being processed (the method should not change this agent state)
      * @param innerContent the content (plans, beliefs, ...) inside the begin/end directive (as in goal patterns)
      * @return the agent (plans, bels, ...) with the result of the directive.
      */
     Agent process(Pred directive, Agent outerContent, Agent innerContent);
-    
+
     /** called when the directive ends */
     void end(Pred directive, as2j parser);
-    
+
     /** returns true if the same instance will be used by all agents/parsers in the JVM */
     boolean isSingleton();
 }
