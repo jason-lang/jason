099fb3f9028179ec5403c77361eaed27aed868d7
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:47 -0300

    my_name.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/stdlib/my_name.java
+++ src/main/java/jason/stdlib/my_name.java
@@ -9,20 +9,20 @@ import jason.asSyntax.Term;
 
 /**
   <p>Internal action: <b><code>.my_name</code></b>.
-  
+
   <p>Description: gets the agent's unique identification in the
   multi-agent system. This identification is given by the runtime
   infrastructure of the system (centralised, saci, jade, ...).
-  
+
   <p>Parameter:<ul>
-  
+
   <li>+/- name (atom): if this is a variable, unifies the agent
   name and the variable; if it is an atom, succeeds if the atom is equal to
   the agent's name.<br/>
 
   </ul>
-  
-  <p>Example:<ul> 
+
+  <p>Example:<ul>
 
   <li> <code>.my_name(N)</code>: unifies <code>N</code> with the
   agent's name.</li>
@@ -36,20 +36,24 @@ import jason.asSyntax.Term;
 
  */
 public class my_name extends DefaultInternalAction {
-	
-	private static InternalAction singleton = null;
-	public static InternalAction create() {
-		if (singleton == null) 
-			singleton = new my_name();
-		return singleton;
-	}
 
-	@Override public int getMinArgs() { return 1; }
-    @Override public int getMaxArgs() { return 1; }
+    private static InternalAction singleton = null;
+    public static InternalAction create() {
+        if (singleton == null)
+            singleton = new my_name();
+        return singleton;
+    }
+
+    @Override public int getMinArgs() {
+        return 1;
+    }
+    @Override public int getMaxArgs() {
+        return 1;
+    }
 
     @Override
-	public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
+    public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
         checkArguments(args);
         return un.unifies(args[0], new Atom(ts.getUserAgArch().getAgName()));
-	}
+    }
 }
