1446d3eac5eefabbaf971984e60ad463f8696d3c
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:47 -0300

    println.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/stdlib/println.java
+++ src/main/java/jason/stdlib/println.java
@@ -12,7 +12,7 @@ import java.util.logging.Level;
 
 /**
   <p>Internal action: <b><code>.println</code></b>.
-  
+
   <p>Description: used for printing messages to the console. Exactly as for
   <code>.print</code> except that a new line is printed after the parameters.
 
@@ -24,25 +24,25 @@ public class println extends DefaultInternalAction {
 
     private static InternalAction singleton = null;
     public static InternalAction create() {
-        if (singleton == null) 
+        if (singleton == null)
             singleton = new println();
         return singleton;
     }
-    
+
     protected String getNewLine() {
         return "\n";
     }
-    
+
     @Override
     public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
         String sout = argsToString(args);
-        
+
         if (ts != null && ts.getSettings().logLevel() != Level.WARNING) {
             ts.getLogger().info(sout.toString());
         } else {
             System.out.print(sout.toString() + getNewLine());
         }
-        
+
         return true;
     }
 
@@ -53,7 +53,7 @@ public class println extends DefaultInternalAction {
         //        sout = new StringBuilder();
         //    }
         //} catch (Exception e) {}
-        
+
         for (int i = 0; i < args.length; i++) {
             if (args[i].isString()) {
                 StringTerm st = (StringTerm)args[i];
