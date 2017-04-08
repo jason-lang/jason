ec8a525b317a3a7e9a837126279e264d28f362da
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:43 -0300

    ListTerm.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/asSyntax/ListTerm.java
+++ src/main/java/jason/asSyntax/ListTerm.java
@@ -5,20 +5,20 @@ import java.util.List;
 
 /**
  * The interface for lists of the AgentSpeak language
- *  
+ *
  * @opt nodefillcolor lightgoldenrodyellow
- * 
+ *
  * @author Jomi
  */
 public interface ListTerm extends java.util.List<Term>, Term {
-    
+
     public void setTerm(Term t);
     public Term getTerm();
     public void setNext(Term l);
     public ListTerm getNext();
-    
+
     public boolean isEnd();
-    
+
     public boolean isTail();
     public VarTerm getTail();
     public void setTail(VarTerm v);
@@ -30,21 +30,21 @@ public interface ListTerm extends java.util.List<Term>, Term {
 
     public ListTerm concat(ListTerm lt);
     public ListTerm reverse();
-    
+
     public ListTerm union(ListTerm lt);
     public ListTerm intersection(ListTerm lt);
     public ListTerm difference(ListTerm lt);
-    
-    /** returns all subsets that take k elements of this list */ 
+
+    /** returns all subsets that take k elements of this list */
     public Iterator<List<Term>> subSets(int k);
 
-    
+
     public Iterator<ListTerm> listTermIterator();
     public List<Term> getAsList();
 
     /** clone the list term */
     public ListTerm cloneLT();
-    
+
     /** make a shallow copy of the list (terms are not cloned, only the structure) */
     public ListTerm cloneLTShallow();
 }
