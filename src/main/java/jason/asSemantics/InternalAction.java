b03e61359086bfeafb4e9094a49bbbd4f32cbb5a
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:43 -0300

    InternalAction.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/asSemantics/InternalAction.java
+++ src/main/java/jason/asSemantics/InternalAction.java
@@ -5,26 +5,26 @@ import jason.asSyntax.Term;
 
 /**
  * Common interface for all internal actions.
- *  
+ *
  * @author Jomi
  */
 public interface InternalAction {
-    
-    /** Returns true if the internal action (IA) should suspend the 
+
+    /** Returns true if the internal action (IA) should suspend the
         intention where the IA is called */
     boolean suspendIntention();
-    
+
     /** Return true if the internal action can be used in plans' context */
     boolean canBeUsedInContext();
-    
+
     /** Prepare body's terms to be used in 'execute', normally it consist of cloning and applying each term */
     public Term[] prepareArguments(Literal body, Unifier un);
-    
+
     /** Executes the internal action. It should return a Boolean or
      *  an Iterator<Unifier>. A true boolean return means that the IA was
-     *  successfully executed. An Iterator result means that there is 
+     *  successfully executed. An Iterator result means that there is
      *  more than one answer for this IA (e.g. see member internal action). */
     Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception;
-    
+
     public void destroy() throws Exception;
 }
