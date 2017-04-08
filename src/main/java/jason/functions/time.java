bdffcf287647f1452e8c947d820b77001992cc86
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:45 -0300

    time.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/functions/time.java
+++ src/main/java/jason/functions/time.java
@@ -4,20 +4,20 @@ import jason.asSemantics.DefaultArithFunction;
 import jason.asSemantics.TransitionSystem;
 import jason.asSyntax.Term;
 
-/** 
+/**
   <p>Function: <b><code>system.time</code></b>: encapsulates java System.currentTimeMillis(),
   returns the current time in milliseconds.
-  
+
   @see jason.stdlib.time internal action time
 
-  @author Jomi 
+  @author Jomi
 */
 public class time extends DefaultArithFunction  {
 
     public String getName() {
         return "system.time";
     }
-    
+
     @Override
     public double evaluate(TransitionSystem ts, Term[] args) throws Exception {
         return System.currentTimeMillis();
@@ -27,5 +27,5 @@ public class time extends DefaultArithFunction  {
     public boolean checkArity(int a) {
         return a == 0;
     }
-    
+
 }
