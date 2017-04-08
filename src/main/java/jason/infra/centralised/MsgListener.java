464c91921e59c027715d111e1df2b5da9de37193
Patrick José Pereira <patrickelectric@gmail.com>
2017-04-08 17:45:45 -0300

    MsgListener.java: Solve code style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main/java/jason/infra/centralised/MsgListener.java
+++ src/main/java/jason/infra/centralised/MsgListener.java
@@ -5,7 +5,7 @@ import jason.asSemantics.*;
 /**
  * Interface for objects that want to listen sent messages in
  * centralised architecture.
- * 
+ *
  * @author Jomi
  */
 public interface MsgListener {
