d6cdd1e62ae585f821ff796af070f9d2cfb254f5
Cleber Jorge Amaral <cleberjamaral@gmail.com>
2018-12-21 00:54:06 -0200

    retention changed to runtime

--- src/main/java/jason/stdlib/Manual.java
+++ src/main/java/jason/stdlib/Manual.java
@@ -2,12 +2,24 @@ package jason.stdlib;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 
 /** Manual for internal actions */
+@Retention(RetentionPolicy.RUNTIME) 
 @Target(ElementType.TYPE) //on class level
 public @interface Manual {
     String    hint()     default "no hint"; 
     String    literal()  default "noliteral(a,b,c)"; 
-    String[]  argsHint() default ""; 
-    String[]  argsType() default ""; 
+    String[]  argsHint() default {  
+                                    "hint for arg a ", 
+                                    "hint for arg b ",
+                                    "hint for arg c "
+                                  }; 
+    String[]  argsType() default {  
+                                    "type of arg a ", 
+                                    "type of arg b ",
+                                    "type of arg c "
+                                };
 }
