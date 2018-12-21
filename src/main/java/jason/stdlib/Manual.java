b2f0d7226142a63ebaff61db7b8c75f75053a7e1
Cleber Jorge Amaral <cleberjamaral@gmail.com>
2018-12-21 00:54:06 -0200

    retention changed to runtime
    removed default hints, if not replaced would confuse users

--- src/main/java/jason/stdlib/Manual.java
+++ src/main/java/jason/stdlib/Manual.java
@@ -2,12 +2,16 @@ package jason.stdlib;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 
 /** Manual for internal actions */
+@Retention(RetentionPolicy.RUNTIME) 
 @Target(ElementType.TYPE) //on class level
 public @interface Manual {
     String    hint()     default "no hint"; 
     String    literal()  default "noliteral(a,b,c)"; 
     String[]  argsHint() default ""; 
-    String[]  argsType() default ""; 
+    String[]  argsType() default "";
 }
