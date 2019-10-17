127f2f82d26131b967f5b0687ad89d4345d44d61
Jomi Fred Hubner <jomifred@gmail.com>
2019-10-17 17:26:20 -0300

    add script for adoc

--- doc/publishFAQ.sh
+++ doc/publishFAQ.sh
@@ -1,4 +1,2 @@
-
-IMAGE=jomifred/adoc
-docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/app "$IMAGE" asciidoctor -r /pygments_init.rb faq.adoc
+../scripts/asciidoc-docker faq.adoc
 scp faq.html  $USERSF,jason@web.sf.net:/home/groups/j/ja/jason/htdocs/doc
