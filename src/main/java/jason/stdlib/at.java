4cbc056039b3427bfdb4cefac56bf0cf2c668aef
Jomi Fred Hubner <jomifred@gmail.com>
2020-05-06 11:29:31 -0300

    add serializable to some classes

--- src/main/java/jason/stdlib/at.java
+++ src/main/java/jason/stdlib/at.java
@@ -1,5 +1,6 @@
 package jason.stdlib;
 
+import java.io.Serializable;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
@@ -139,7 +140,7 @@ public class at extends DefaultInternalAction {
             t.cancel();
     }
 
-    class CheckDeadline implements Runnable {
+    class CheckDeadline implements Runnable, Serializable {
         private int     id = 0;
         private Event   event;
         private TransitionSystem ts;
