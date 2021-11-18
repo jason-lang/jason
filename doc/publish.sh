726bfdf8a5fc62c5cf05cc0b00d9f28f70a47cda
Jomi Fred Hubner <jomifred@gmail.com>
2021-11-18 18:15:53 -0300

    update required java version in doc

--- doc/publish.sh
+++ doc/publish.sh
@@ -3,8 +3,8 @@
 #
 
 cd ..
-gradle renderAsciidoc
-gradle javadoc
+./gradlew renderAsciidoc
+./gradlew javadoc
 cd doc
 cp readme.html index.html
 scp -r *  $USERSF,jason@web.sf.net:/home/groups/j/ja/jason/htdocs/doc
