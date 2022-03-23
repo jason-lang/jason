238a7a2a793a5edc3f1c67541d4dbebbefb34abe
Jomi Fred Hubner <jomifred@gmail.com>
2022-03-23 11:38:38 -0300

    start 3.1

--- src/main/resources/scripts/publish.sh
+++ src/main/resources/scripts/publish.sh
@@ -3,7 +3,7 @@ rm -rf .gradle
 zip -r ../np31.zip *
 cd ..
 
-#scp np31.zip $USERSF,jason@web.sf.net:/home/project-web/jason/htdocs/np
+scp np31.zip $USERSF,jason@web.sf.net:/home/project-web/jason/htdocs/np
 
 #scp *.zip $USERSF,jacamo@web.sf.net:/home/project-web/jacamo/htdocs/nps
 
