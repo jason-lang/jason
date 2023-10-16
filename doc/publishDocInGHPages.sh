36b33ca425aa20c1c9880fd5c5105a0a67035716
Jomi Fred Hubner <jomifred@gmail.com>
2023-10-16 12:07:20 +0200

    add jason github doc folder

---
+++ doc/publishDocInGHPages.sh
@@ -0,0 +1,24 @@
+cd ..
+
+#./gradlew doc
+
+rm -rf /tmp/gh-pages*
+git clone https://github.com/jason-lang/jason.git --branch gh-pages-new --single-branch /tmp/gh-pages
+
+cp -R jason-interpreter/build/docs/javadoc /tmp/gh-pages/api
+
+cp -R doc /tmp/gh-pages/doc
+
+
+cd /tmp/gh-pages
+git add api
+git commit -a -m "add javadoc api"
+
+find doc -name readme.html -execdir cp readme.html index.html \;
+git add doc
+git commit -a -m "add jason github doc folder"
+
+git push
+
+
+
