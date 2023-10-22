2ee5580e39cd6d690d1779e11fcd7baf485098c3
Jomi Fred Hubner <jomifred@gmail.com>
2023-10-22 12:16:59 +0200

    update readme

--- doc/publishDocInGHPages.sh
+++ doc/publishDocInGHPages.sh
@@ -2,23 +2,19 @@ cd ..
 
 ./gradlew doc
 
-#rm -rf /tmp/gh-pages*
-#git clone https://github.com/jason-lang/jason.git --branch gh-pages-new --single-branch /tmp/gh-pages
-
 rm -rf ../jason-pages/api
-#rm -rf ../jason-pages/doc
+rm -rf ../jason-pages/doc
 
 cp -R jason-interpreter/build/docs/javadoc ../jason-pages/api
-#cp -R doc ../jason-pages
-
+cp -R doc ../jason-pages
 
 cd ../jason-pages
 git add api
 git commit -a -m "add javadoc api"
 
-#find doc -name readme.html -execdir cp readme.html index.html \;
-#git add doc
-#git commit -a -m "add jason github doc folder"
+find doc -name readme.html -execdir cp readme.html index.html \;
+git add doc
+git commit -a -m "add jason github doc folder"
 
 git push
 
