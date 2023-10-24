de8e36da5b683bdcda9366e39d896d1d1795b77b
Jomi Fred Hubner <jomifred@gmail.com>
2023-10-24 12:37:16 +0200

    update script for site

--- doc/publishDocInGHPages.sh
+++ doc/publishDocInGHPages.sh
@@ -5,7 +5,9 @@ cd ..
 rm -rf ../jason-pages/api
 rm -rf ../jason-pages/doc
 
-asciidoctor -a stylesheet=adoc-empty.css readme.adoc -o doc.html
+#asciidoctor -a stylesheet=adoc-empty.css readme.adoc -o doc.html
+IMAGE=jomifred/adoc
+docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/app "$IMAGE" asciidoctor -r /pygments_init.rb -a stylesheet=adoc-empty.css readme.adoc -o doc.html
 
 cp -R jason-interpreter/build/docs/javadoc ../jason-pages/api
 cp -R doc ../jason-pages
