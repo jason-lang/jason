835d60254ada5ca5cb04a96cf418011343a1178f
Jomi Fred Hubner <jomifred@gmail.com>
2023-10-24 12:30:06 +0200

    update script to publish pages

--- doc/publishDocInGHPages.sh
+++ doc/publishDocInGHPages.sh
@@ -5,6 +5,8 @@ cd ..
 rm -rf ../jason-pages/api
 rm -rf ../jason-pages/doc
 
+asciidoctor -a stylesheet=adoc-empty.css readme.adoc -o doc.html
+
 cp -R jason-interpreter/build/docs/javadoc ../jason-pages/api
 cp -R doc ../jason-pages
 
@@ -12,7 +14,7 @@ cd ../jason-pages
 git add api
 git commit -a -m "add javadoc api"
 
-find doc -name readme.html -execdir cp readme.html index.html \;
+#find doc -name readme.html -execdir cp readme.html index.html \;
 git add doc
 git commit -a -m "add jason github doc folder"
 
